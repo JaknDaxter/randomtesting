156fa79328391c3f619c268237fdf5daf6e0d818
Per Berglund <Per.berglund92@gmail.com>
2018-05-29 15:06:29 +0200

    added a yellow bg

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
+
 <!doctype html>
 <html>
     <head>
         <title>Example form</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="background.css">
     </head>
     <body>
         <div class="container">
